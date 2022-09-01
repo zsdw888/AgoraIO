AgoraIO-Extensions/agora_rtc_ng_flutter

feat: Upgrade native sdk 4.0.0-rc.1

lib/src/binding/agora_media_player_event_impl.dart
@@ -1,0 +1,93 @@
+import 'package:agora_rtc_ng/src/binding_forward_export.dart';
+import 'package:agora_rtc_ng/src/binding/impl_forward_export.dart';
+import 'package:iris_event/iris_event.dart';
+
+// ignore_for_file: public_member_api_docs, unused_local_variable
+
+extension MediaPlayerAudioFrameObserverExt on MediaPlayerAudioFrameObserver {
+  void process(String event, String data, List<Uint8List> buffers) {
+    final jsonMap = jsonDecode(data);
+    switch (event) {
+      case 'MediaPlayerAudioFrameObserver_onFrame':
+        if (onFrame == null) break;
+        MediaPlayerAudioFrameObserverOnFrameJson paramJson =
+            MediaPlayerAudioFrameObserverOnFrameJson.fromJson(jsonMap);
+        paramJson = paramJson.fillBuffers(buffers);
+        AudioPcmFrame? frame = paramJson.frame;
+        if (frame == null) {
+          break;
+        }
+        frame = frame.fillBuffers(buffers);
+        onFrame!(frame);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+class MediaPlayerAudioFrameObserverWrapper implements IrisEventHandler {
+  const MediaPlayerAudioFrameObserverWrapper(
+      this.mediaPlayerAudioFrameObserver);
+  final MediaPlayerAudioFrameObserver mediaPlayerAudioFrameObserver;
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is MediaPlayerAudioFrameObserverWrapper &&
+        other.mediaPlayerAudioFrameObserver == mediaPlayerAudioFrameObserver;
+  }
+
+  @override
+  int get hashCode => mediaPlayerAudioFrameObserver.hashCode;
+  @override
+  void onEvent(String event, String data, List<Uint8List> buffers) {
+    if (!event.startsWith('MediaPlayerAudioFrameObserver')) return;
+    mediaPlayerAudioFrameObserver.process(event, data, buffers);
+  }
+}
+
+extension MediaPlayerVideoFrameObserverExt on MediaPlayerVideoFrameObserver {
+  void process(String event, String data, List<Uint8List> buffers) {
+    final jsonMap = jsonDecode(data);
+    switch (event) {
+      case 'MediaPlayerVideoFrameObserver_onFrame':
+        if (onFrame == null) break;
+        MediaPlayerVideoFrameObserverOnFrameJson paramJson =
+            MediaPlayerVideoFrameObserverOnFrameJson.fromJson(jsonMap);
+        paramJson = paramJson.fillBuffers(buffers);
+        VideoFrame? frame = paramJson.frame;
+        if (frame == null) {
+          break;
+        }
+        frame = frame.fillBuffers(buffers);
+        onFrame!(frame);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+class MediaPlayerVideoFrameObserverWrapper implements IrisEventHandler {
+  const MediaPlayerVideoFrameObserverWrapper(
+      this.mediaPlayerVideoFrameObserver);
+  final MediaPlayerVideoFrameObserver mediaPlayerVideoFrameObserver;
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is MediaPlayerVideoFrameObserverWrapper &&
+        other.mediaPlayerVideoFrameObserver == mediaPlayerVideoFrameObserver;
+  }
+
+  @override
+  int get hashCode => mediaPlayerVideoFrameObserver.hashCode;
+  @override
+  void onEvent(String event, String data, List<Uint8List> buffers) {
+    if (!event.startsWith('MediaPlayerVideoFrameObserver')) return;
+    mediaPlayerVideoFrameObserver.process(event, data, buffers);
+  }
+}

lib/src/impl/video_view_controller_impl.dart
@@ -2,6 +2,7 @@ import 'package:agora_rtc_ng/src/agora_rtc_engine.dart';
 import 'package:agora_rtc_ng/src/agora_base.dart';
 import 'package:agora_rtc_ng/src/agora_rtc_engine_ex.dart';
 import 'package:agora_rtc_ng/src/impl/agora_rtc_engine_impl.dart';
+import 'package:agora_rtc_ng/src/render/video_view_controller.dart';
 import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 
@@ -34,45 +35,6 @@ extension VideoViewControllerBaseExt on VideoViewControllerBase {
       (defaultTargetPlatform == TargetPlatform.macOS ||
           defaultTargetPlatform == TargetPlatform.windows) ||
       useFlutterTexture;
-}
-
-abstract class VideoViewControllerBase {
-  RtcEngine get rtcEngine;
-
-  VideoCanvas get canvas;
-
-  RtcConnection? get connection;
-
-  bool get useFlutterTexture;
-
-  bool get useAndroidSurfaceView;
-
-  @internal
-  void setTextureId(int textureId);
-
-  @internal
-  int getTextureId();
-
-  @internal
-  int getVideoSourceType();
-
-  @internal
-  Future<void> setupView(int nativeViewPtr);
-
-  @protected
-  Future<int> createTextureRender(
-    int uid,
-    String channelId,
-    int videoSourceType,
-  );
-
-  @internal
-  Future<void> initialize();
-
-  @internal
-  Future<void> disposeRender();
-
-  Future<void> dispose();
 }
 
 mixin VideoViewControllerBaseMixin implements VideoViewControllerBase {
@@ -133,7 +95,7 @@ mixin VideoViewControllerBaseMixin implements VideoViewControllerBase {
   }
 
   @override
-  Future<void> initialize() async {
+  Future<void> initializeRender() async {
     if (shouldUseFlutterTexture) {
       if (_textureId == kTextureNotInit) {
         _textureId = await createTextureRender(

zsdw888/AgoraIO

lib/src/binding/agora_media_player_event_impl.dart
@@ -1,0 +1,93 @@
+import 'package:agora_rtc_engine/src/binding_forward_export.dart';
+import 'package:agora_rtc_engine/src/binding/impl_forward_export.dart';
+import 'package:iris_event/iris_event.dart';
+
+// ignore_for_file: public_member_api_docs, unused_local_variable
+
+extension MediaPlayerAudioFrameObserverExt on MediaPlayerAudioFrameObserver {
+  void process(String event, String data, List<Uint8List> buffers) {
+    final jsonMap = jsonDecode(data);
+    switch (event) {
+      case 'MediaPlayerAudioFrameObserver_onFrame':
+        if (onFrame == null) break;
+        MediaPlayerAudioFrameObserverOnFrameJson paramJson =
+            MediaPlayerAudioFrameObserverOnFrameJson.fromJson(jsonMap);
+        paramJson = paramJson.fillBuffers(buffers);
+        AudioPcmFrame? frame = paramJson.frame;
+        if (frame == null) {
+          break;
+        }
+        frame = frame.fillBuffers(buffers);
+        onFrame!(frame);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+class MediaPlayerAudioFrameObserverWrapper implements IrisEventHandler {
+  const MediaPlayerAudioFrameObserverWrapper(
+      this.mediaPlayerAudioFrameObserver);
+  final MediaPlayerAudioFrameObserver mediaPlayerAudioFrameObserver;
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is MediaPlayerAudioFrameObserverWrapper &&
+        other.mediaPlayerAudioFrameObserver == mediaPlayerAudioFrameObserver;
+  }
+
+  @override
+  int get hashCode => mediaPlayerAudioFrameObserver.hashCode;
+  @override
+  void onEvent(String event, String data, List<Uint8List> buffers) {
+    if (!event.startsWith('MediaPlayerAudioFrameObserver')) return;
+    mediaPlayerAudioFrameObserver.process(event, data, buffers);
+  }
+}
+
+extension MediaPlayerVideoFrameObserverExt on MediaPlayerVideoFrameObserver {
+  void process(String event, String data, List<Uint8List> buffers) {
+    final jsonMap = jsonDecode(data);
+    switch (event) {
+      case 'MediaPlayerVideoFrameObserver_onFrame':
+        if (onFrame == null) break;
+        MediaPlayerVideoFrameObserverOnFrameJson paramJson =
+            MediaPlayerVideoFrameObserverOnFrameJson.fromJson(jsonMap);
+        paramJson = paramJson.fillBuffers(buffers);
+        VideoFrame? frame = paramJson.frame;
+        if (frame == null) {
+          break;
+        }
+        frame = frame.fillBuffers(buffers);
+        onFrame!(frame);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+class MediaPlayerVideoFrameObserverWrapper implements IrisEventHandler {
+  const MediaPlayerVideoFrameObserverWrapper(
+      this.mediaPlayerVideoFrameObserver);
+  final MediaPlayerVideoFrameObserver mediaPlayerVideoFrameObserver;
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is MediaPlayerVideoFrameObserverWrapper &&
+        other.mediaPlayerVideoFrameObserver == mediaPlayerVideoFrameObserver;
+  }
+
+  @override
+  int get hashCode => mediaPlayerVideoFrameObserver.hashCode;
+  @override
+  void onEvent(String event, String data, List<Uint8List> buffers) {
+    if (!event.startsWith('MediaPlayerVideoFrameObserver')) return;
+    mediaPlayerVideoFrameObserver.process(event, data, buffers);
+  }
+}

lib/src/impl/video_view_controller_impl.dart
@@ -1,7 +1,7 @@
-import 'package:agora_rtc_ng/src/agora_rtc_engine.dart';
-import 'package:agora_rtc_ng/src/agora_base.dart';
-import 'package:agora_rtc_ng/src/agora_rtc_engine_ex.dart';
-import 'package:agora_rtc_ng/src/impl/agora_rtc_engine_impl.dart';
+import 'package:agora_rtc_engine/src/agora_base.dart';
+import 'package:agora_rtc_engine/src/agora_rtc_engine_ex.dart';
+import 'package:agora_rtc_engine/src/impl/agora_rtc_engine_impl.dart';
+import 'package:agora_rtc_engine/src/render/video_view_controller.dart';
 import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 
@@ -34,45 +34,6 @@ extension VideoViewControllerBaseExt on VideoViewControllerBase {
       (defaultTargetPlatform == TargetPlatform.macOS ||
           defaultTargetPlatform == TargetPlatform.windows) ||
       useFlutterTexture;
-}
-
-abstract class VideoViewControllerBase {
-  RtcEngine get rtcEngine;
-
-  VideoCanvas get canvas;
-
-  RtcConnection? get connection;
-
-  bool get useFlutterTexture;
-
-  bool get useAndroidSurfaceView;
-
-  @internal
-  void setTextureId(int textureId);
-
-  @internal
-  int getTextureId();
-
-  @internal
-  int getVideoSourceType();
-
-  @internal
-  Future<void> setupView(int nativeViewPtr);
-
-  @protected
-  Future<int> createTextureRender(
-    int uid,
-    String channelId,
-    int videoSourceType,
-  );
-
-  @internal
-  Future<void> initialize();
-
-  @internal
-  Future<void> disposeRender();
-
-  Future<void> dispose();
 }
 
 mixin VideoViewControllerBaseMixin implements VideoViewControllerBase {
@@ -133,7 +94,7 @@ mixin VideoViewControllerBaseMixin implements VideoViewControllerBase {
   }
 
   @override
-  Future<void> initialize() async {
+  Future<void> initializeRender() async {
     if (shouldUseFlutterTexture) {
       if (_textureId == kTextureNotInit) {
         _textureId = await createTextureRender(